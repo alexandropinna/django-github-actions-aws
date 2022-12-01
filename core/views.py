a6f2c9cd2425b4aa2a6d0e8f3e9038679af321c9
alejandro <alexander.pinna@protonmail.com>
2022-12-01 20:39:05 +0000

    prueba de cambio de permisos 1.0.2

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 
 def home(request):
-   text = """<h1>Hello world cambio 1.0.1</h1>"""
+   text = """<h1>Hello world cambio 1.0.2</h1>"""
    return HttpResponse(text)
